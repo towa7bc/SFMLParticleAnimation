f64ffebf85c1420c05f454bfe171682e99f86b71
MW <michi.wittmann@gmail.com>
2020-06-05 15:08:54 +0200

    Fix uint to int bug in Particle.hpp 2

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -14,7 +14,7 @@ void Particle::draw(sf::RenderTarget &target, sf::RenderStates states) const {
 }
 
 void Particle::updateDrawVertexColorAlpha(const int &alpha) {
-  draw_vertex_.color.a -= static_cast<sf::Uint8>(alpha);
+  draw_vertex_.color.a -= static_cast<unsigned char>(alpha);
 }
 
 void Particle::updateVelocity(const sf::Vector2f &vel) { velocity_ += vel; }
